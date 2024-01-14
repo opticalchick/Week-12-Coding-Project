f7c42eacec12eb8843699306a786f135f5ec07a3
Becky Van Vickle <opticalchick@yahoo.com>
2024-01-14 13:17:02 -0600

    still working on coding and functionality

--- index.js
+++ index.js
@@ -1,97 +1,42 @@
-class ToolType {
-    constructor(name) {
-        this.name = name;
-        this.tools = [];
-    }
+// class Tool {
+//     constructor(name) {
+//         this.name = name;
+//         this.quantity = quantity;
+//         this.category = category;
+//         this.tools = [];
+//     }
 
-    addTool(name, quantity) {
-        this.tools.push(new Tool(name, quantity));
-    }
+//     addTool(name, quantity, category) {
+//         this.tools.push(new Tool(name, quantity, category));
+//     }
+// }
+
+
+
+const TOOLS_URL = 'https://659d7119633f9aee79096f5e.mockapi.io/PTapi/tool';
+
+async function getTools() {
+    fetch(TOOLS_URL)
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch((error) => console.error('Error:', error))
 }
 
-class Tool {
-    constructor(name, quantity) {
-        this.name = name;
-        this.quantity = quantity;
-    }
-}
+getTools();
 
-class ToolTypeService {
-    static url = 'https://659d7119633f9aee79096f5e.mockapi.io/PTapi/toolTypes';
+$.get(TOOLS_URL).then(data => {
+    data.map(tool => {
+        $('tbody').append(
+            $(`
+            <tr>
+                <td>${tool.id}</td>
+                <td>${tool.name}</td>
+                <td>${tool.quantity}</td>
+                <td>${tool.category}</td>
+                <td>
+                    <button onClick="deleteUer(${tool.id})" class="btn btn-danger">Delete</button>                    
+            `)
+        )
+    })
+})
 
-    static getAllToolTypes() {
-        return $.get(this.url);
-    }
-
-    static getToolType(id) {
-        return $.get(this.url + `${id}`);
-    }
-
-    static createToolType(toolType) {
-        return $.post(this.url, toolType);
-    }
-
-    static updateToolType(toolType) {
-        return $.ajax({
-            url: this.url + '/${toolType.id}',
-            dataType: 'json',
-            data: JSON.stringify(toolType),
-            contentType: 'application/json',
-            type: 'PUT'
-        });
-    }
-
-    static deleteToolType(id) {
-        return $.ajax({
-            url: this.url + '/${id}',
-            type: 'DELETE'
-        });
-    }
-}
-
-class DOMManager {
-    static toolType;
-
-    static getAllToolTypes() {
-        ToolTypeService.getAllToolTypes().then(toolTypes => this.render(toolTypes));
-    }
-
-    static render(toolTypes) {
-        this.toolTypes = toolTypes;
-        $('#app').empty();
-        for (let toolType of toolTypes) {
-            $('#app').append(
-                ` <div id="${toolType.id}" class="card bg-light">
-                    <div class="card-header text-center">
-                        <h3>${toolType.name}</h3>
-                        <button class="btn btn-danger" onclick="DOMManager.deleteToolType('${toolType.id}')">Delete</button>
-                    </div>
-                    <div class="card-body">
-                        <div class="card">
-                            <div class="row">
-                                <div class="col-sm">
-                                <input type="text" id="${toolType.id}-tool-name" class="form-control" placeholder="Tool Name">
-                                </div>
-                                <div class="col-sm">
-                                <input type="text" id="${toolType.id}-tool-quantity" class="form-control" placeholder="Quantity">
-                                </div>
-                            </div>
-                            <br>
-                            <button id="${toolType.id}-new-tool" onclick="DOMManager.addTool('${toolType.id}')" class="btn btn-info ">Add Tool</button>
-                        </div>
-                    </div>
-                  </div><br>`
-            );
-            for (let tool of toolType.tools) {
-                $(`#${toolType.id}`).find('.card-body').append(
-                    `<p>
-                    <span id ="name-${toolName.id}"><strong>Name:  </strong> ${toolName.name}</span>
-                    <span id ="name-${toolName.id}"><strong>Quantity:  </strong> ${toolName.quantity}</span>
-                    <button class="btn btn-danger" onclick="DOMManager.deleteTool('${toolType.id}', '${toolName.id})">Delete Tool</button>`
-                );
-            }
-        }
-    }
-}
-
-DOMManager.getAllToolTypes();
